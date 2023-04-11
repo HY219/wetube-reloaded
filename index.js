bea5e07c02a0fe6bae78b6b6494e7a6308184c3f
hy219 <jhy_0219@daum.net>
2023-04-11 20:21:29 +0900

    babel preset-env, nodemon 설치(최신JS를 이해할 수 있도록 컴파일 -> nodeJS가 이해하도록)

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello NodeJS");
+//express 어플리케이션을 만들었다.
+const express = require("express");
+
+const app = express();
+
+//Babel : JS컴파일러 -> 최신JS를 컴파일해준다.(nodeJS가 이해하도록 바꿔줌)
+//nodeJS가 이해하지 못하는 최신JS코드가 있다.
