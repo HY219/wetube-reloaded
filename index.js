eebbc4072acdb227843e17451c2bd6d3caaaa6df
hy219 <jhy_0219@daum.net>
2023-04-11 20:40:13 +0900

    최신 js로 수정하기

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //express 어플리케이션을 만들었다.
-const express = require("express");
+// const express = require("express");
+import express from "express";
 
 const app = express();
 
 //Babel : JS컴파일러 -> 최신JS를 컴파일해준다.(nodeJS가 이해하도록 바꿔줌)
 //nodeJS가 이해하지 못하는 최신JS코드가 있다.
+
+console.log("hi");
