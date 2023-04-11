ec3ee70f989192abe5878717fff2196a68bffe64
hy219 <jhy_0219@daum.net>
2023-04-12 02:42:51 +0900

    get request 보내기

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import express from "express"; //node_modules에서 express를 찾는 것을 알 수 있다. //express의 index.js를 불러온다.
 
+const PORT = 4000;
+
 const app = express(); //express function을 사용하면 express application을 생성해준다. //app이 express application이 되었다.
 
-const handleListening = () => console.log("Server listening on port 4000");
+//request 관련 코드 여기에 작성
+app.get("/", () => console.log("Somebody is trying to go home.")); //"/"(home)에 get request를 보낸다.
 
-app.listen(4000, handleListening); //callback : server가 시작될 때 실행되는 함수 //port, callback함수
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening); //callback : server가 시작될 때 실행되는 함수 //port, callback함수
+//button.addEventListener("click", handleClick)
