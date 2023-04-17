3410280c5819e253aa210a97b31342ad87e3340f
hy219 <jhy_0219@daum.net>
2023-04-17 18:06:06 +0900

    middleware를 사용해서 private페이지에 접근하지 못하도록 적용

--- src/server.js
+++ src/server.js
@@ -5,16 +5,33 @@ const PORT = 4000;
 const app = express(); //express function을 사용하면 express application을 생성해준다. //app이 express application이 되었다.
 
 //controller(=middleware)에 next인자(argument) 추가
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Some one is going to : ${req.url}`); //req가 request에 관한 정보를 가지고 있는 object이다.
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`); //req가 request에 관한 정보를 가지고 있는 object이다.
   next(); //다음 함수를 호출해준다.
 };
-const handleHome = (req, res, next) => {
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("You made continue.");
+  next();
+};
+
+const handleHome = (req, res) => {
   return res.send("I love middle ware");
 };
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to Private lounge.");
+};
+
 //순서 중요 (use 다음 URL에서 get)
-app.use(gossipMiddleware); //모든 route에 함수를 사용(적용)한다.
-app.get("/", handleHome); //"/"(home)에 get request를 보낸다.
+app.use(logger); //모든 route에 함수를 사용(적용)한다.
+app.use(privateMiddleware);
+app.get("/", handleHome, logger); //"/"(home)에 get request를 보낸다.
+app.get("/protected", handleProtected, logger);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
