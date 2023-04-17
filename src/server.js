59d396a3bd0d519af90d6811e84dfa42f4c29336
hy219 <jhy_0219@daum.net>
2023-04-17 17:57:39 +0900

    [#3.6 middleware] app.use(모든 route에 적용)

--- src/server.js
+++ src/server.js
@@ -6,13 +6,15 @@ const app = express(); //express function을 사용하면 express application을
 
 //controller(=middleware)에 next인자(argument) 추가
 const gossipMiddleware = (req, res, next) => {
-  console.log(`Some one is going to : ${req.url}`);
+  console.log(`Some one is going to : ${req.url}`); //req가 request에 관한 정보를 가지고 있는 object이다.
   next(); //다음 함수를 호출해준다.
 };
 const handleHome = (req, res, next) => {
   return res.send("I love middle ware");
 };
-app.get("/", gossipMiddleware, handleHome); //"/"(home)에 get request를 보낸다.
+//순서 중요 (use 다음 URL에서 get)
+app.use(gossipMiddleware); //모든 route에 함수를 사용(적용)한다.
+app.get("/", handleHome); //"/"(home)에 get request를 보낸다.
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
