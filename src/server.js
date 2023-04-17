827f0374b6b9080fb365c5d750e8c921df4ce7ef
hy219 <jhy_0219@daum.net>
2023-04-17 19:07:01 +0900

    [#3.11 External Middlewares] 시작 전 세팅

--- src/server.js
+++ src/server.js
@@ -5,33 +5,17 @@ const PORT = 4000;
 const app = express(); //express function을 사용하면 express application을 생성해준다. //app이 express application이 되었다.
 
 //controller(=middleware)에 next인자(argument) 추가
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`); //req가 request에 관한 정보를 가지고 있는 object이다.
-  next(); //다음 함수를 호출해준다.
+const home = (req, res) => {
+  console.log("I will respond."); //req가 request에 관한 정보를 가지고 있는 object이다.
+  return res.send("hello");
 };
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("You made continue.");
-  next();
+const login = (req, res) => {
+  return res.send("login");
 };
 
-const handleHome = (req, res) => {
-  return res.send("I love middle ware");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to Private lounge.");
-};
-
-//순서 중요 (use 다음 URL에서 get)
-app.use(logger); //모든 route에 함수를 사용(적용)한다.
-app.use(privateMiddleware);
-app.get("/", logger, handleHome); //"/"(home)에 get request를 보낸다.
-app.get("/protected", logger, handleProtected);
+app.get("/", home); //"/"(home)에 get request를 보낸다.
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
