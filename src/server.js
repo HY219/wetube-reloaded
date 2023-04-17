f1c2fed16f4c056f05cabd1b5761c51c58b1e1db
hy219 <jhy_0219@daum.net>
2023-04-17 19:49:41 +0900

    morgan

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express"; //node_modules에서 express를 찾는 것을 알 수 있다. //express의 index.js를 불러온다.
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express(); //express function을 사용하면 express application을 생성해준다. //app이 express application이 되었다.
+const logger = morgan("dev");
 
 //controller(=middleware)에 next인자(argument) 추가
 const home = (req, res) => {
@@ -14,6 +16,8 @@ const login = (req, res) => {
   return res.send("login");
 };
 
+// app.use(morgan("dev"));
+app.use(logger);
 app.get("/", home); //"/"(home)에 get request를 보낸다.
 app.get("/login", login);
 
